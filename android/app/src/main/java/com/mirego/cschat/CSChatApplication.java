8fd62d886924b86c7bfeaa8b760d56377978ac21
Nicolas Presseault <npresseault@mirego.com>
2017-03-23 19:26:26 -0400

    Calligraphy

--- android/app/src/main/java/com/mirego/cschat/CSChatApplication.java
+++ android/app/src/main/java/com/mirego/cschat/CSChatApplication.java
@@ -2,6 +2,8 @@ package com.mirego.cschat;
 
 import android.app.Application;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 public class CSChatApplication extends Application {
 
     private ApplicationComponent component;
@@ -12,6 +14,12 @@ public class CSChatApplication extends Application {
 
         component = DaggerApplicationComponent.builder()
                 .build();
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(getString(R.string.font_lato_regular))
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
     }
 
     public ApplicationComponent component() {
--- android/app/src/main/java/com/mirego/cschat/activities/LoginActivity.java
+++ android/app/src/main/java/com/mirego/cschat/activities/LoginActivity.java
@@ -1,4 +1,16 @@
 package com.mirego.cschat.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mirego.cschat.CSChatApplication;
+
 public class LoginActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ((CSChatApplication) getApplication()).component().inject(this);
+    }
 }
