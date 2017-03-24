575df2ecb232705202a668d3652c3e3548ebef98
Nicolas Presseault <npresseault@mirego.com>
2017-03-23 23:23:35 -0400

    Conversations

--- android/app/src/main/java/com/mirego/cschat/activities/ConversationsActivity.java
+++ android/app/src/main/java/com/mirego/cschat/activities/ConversationsActivity.java
@@ -2,11 +2,13 @@ package com.mirego.cschat.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 import com.mirego.cschat.CSChatApplication;
 import com.mirego.cschat.R;
@@ -15,13 +17,22 @@ import com.mirego.cschat.controller.ConversationsController;
 import com.mirego.cschat.controller.LoginController;
 import com.mirego.cschat.viewdatas.ConversationViewData;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class ConversationsActivity extends BaseActivity implements ConversationAdapter.ConversationAdapterListener {
+
+    @BindView(R.id.conversations_root)
+    ViewGroup root;
 
     @BindView(R.id.rv_conversations)
     RecyclerView rvConversations;
@@ -53,6 +64,21 @@ public class ConversationsActivity extends BaseActivity implements ConversationA
         conversationAdapter = new ConversationAdapter(this);
         rvConversations.setLayoutManager(new LinearLayoutManager(this));
         rvConversations.setAdapter(conversationAdapter);
+
+        conversationsController.getConversations()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(new Consumer<List<ConversationViewData>>() {
+                    @Override
+                    public void accept(@NonNull List<ConversationViewData> conversationViewDatas) throws Exception {
+                        conversationAdapter.populateConversations(conversationViewDatas);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(@NonNull Throwable throwable) throws Exception {
+                        Snackbar.make(root, R.string.network_error, Snackbar.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
@@ -73,7 +99,7 @@ public class ConversationsActivity extends BaseActivity implements ConversationA
 
     @OnClick(R.id.fab_add_conversation)
     public void onAddConversationClicked() {
-        //TODO:
+        // TODO:
     }
 
     @Override
