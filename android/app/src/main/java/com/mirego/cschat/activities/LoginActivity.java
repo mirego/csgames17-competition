090a42ccef36d33e661b15ac4338597c18636f2d
Nicolas Presseault <npresseault@mirego.com>
2017-03-23 21:04:31 -0400

    Login btn

--- android/app/src/main/java/com/mirego/cschat/activities/LoginActivity.java
+++ android/app/src/main/java/com/mirego/cschat/activities/LoginActivity.java
@@ -2,11 +2,21 @@ package com.mirego.cschat.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
 
 import com.mirego.cschat.CSChatApplication;
 import com.mirego.cschat.R;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.et_username)
+    EditText etUsername;
+
+    @BindView(R.id.et_password)
+    EditText etPassword;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,5 +25,10 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
 
         ((CSChatApplication) getApplication()).component().inject(this);
+    }
+
+    @OnClick
+    void onLogin() {
+
     }
 }
