6225a756e3c80753f30dc1c8ccb1c62345b04337
Nicolas Presseault <npresseault@mirego.com>
2017-03-23 20:26:39 -0400

    Login wip

--- android/app/src/main/java/com/mirego/cschat/activities/LoginActivity.java
+++ android/app/src/main/java/com/mirego/cschat/activities/LoginActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.mirego.cschat.CSChatApplication;
+import com.mirego.cschat.R;
 
 public class LoginActivity extends BaseActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
 
         ((CSChatApplication) getApplication()).component().inject(this);
     }
