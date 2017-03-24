5ff563c8a190aad61f57d88c8ee0f545b532d018
Jean-Simon Bonin <jsbonin@mirego.com>
2017-03-23 20:26:58 -0400

    wip

--- android/app/src/main/java/com/mirego/cschat/activities/HomeActivity.java
+++ android/app/src/main/java/com/mirego/cschat/activities/HomeActivity.java
@@ -1,6 +1,21 @@
 package com.mirego.cschat.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mirego.cschat.CSChatApplication;
+import com.mirego.cschat.R;
+
+import butterknife.ButterKnife;
+
 public class HomeActivity extends BaseActivity {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ((CSChatApplication) getApplication()).component().inject(this);
+        ButterKnife.bind(this);
+    }
 
 }
