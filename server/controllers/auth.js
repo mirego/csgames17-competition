8a33074553e7e959d0f9146b402e2aa1c3a65eca
Olivier Falardeau <ofalardeau@mirego.com>
2017-03-24 20:13:37 -0400

    now hashing passwords hihi xoxo

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,15 @@
+const bcrypt = require('bcryptjs');
 
 exports.login = function (req, res) {
   let filter = {
-    'username': req.body.username.toLowerCase(),
-    'password': req.body.password
+    'username': req.body.username.toLowerCase()
   };
-  let projection = {
-    password: 0
-  };
+
+  let projection = {};
+
   req.db.users.findOne(filter, projection, function (e, user) {
-    if (user) {
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      delete user.password;
       res.json(user);
     } else {
       res.status(401).send('Username or password invalid');
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 exports.list = function (req, res) {
   let filter = {};
   let projection = {
@@ -28,7 +30,7 @@ exports.register = function (req, res) {
   if (req.body.username && req.body.password) {
     let user = {
       'username': req.body.username,
-      'password': req.body.password,
+      'password': bcrypt.hashSync(req.body.password, 8),
       'url': req.body.url || ''
     };
     req.db.users.insert(user, function (e, user) {
